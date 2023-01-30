a6858647f547a68760cd9f7d200457f8caa3a766
PigeonSenpai <111790515+PigeonSenpai@users.noreply.github.com>
2023-01-29 21:03:39 -0800

    Add files via upload

---
+++ Project/js/scrolling.js
@@ -0,0 +1,26 @@
+let r_text = new Array ();
+r_text[0] = "Wow this movie was so cool!";
+r_text[1] = "This film is an epic in the truest sense and easily one of Pigeon's best Big Films.";
+r_text[2] = "phenomenal movie";
+r_text[3] = "This movie is really great, The animation Beautiful even for 2016 standards.";
+r_text[4] = "Absolutely amazing one of the best movies ever";
+r_text[5] = "Sometimes animations covey more serious massages than movies.";
+r_text[6] = "amazing animated";
+r_text[7] = "My little brother used to be absolutely obsessed with this movie.";
+r_text[8] = "O....M....Beautiful! ";
+r_text[9] = "All I could ask for in a movie.";
+r_text[10] = "a beautiful story";
+r_text[11] = "Personally it’s one of my favorite movies";
+r_text[12] = " It didn't really sound like something which I'd be interested in";
+r_text[13] = "WOW this is a cool movie but that not all";
+r_text[14] = "The most breathtaking computer animation you have seen until now";
+r_text[15] = "I absolutely love this movie.";
+r_text[16] = "I actually watch this movie on theathers with my dad when i was really young, back then i didn't really like it but i enjoyed it.";
+r_text[17] = "This is the most beautiful animated movie ever made! Such beauty";
+r_text[18] = "This has gotta be one of the best films of 2016.";
+r_text[19] = "This has to be one of the greatest animated films ever made, its flawless!";
+r_text[20] = "Great movie, excellent animation.";
+let i = Math.floor(Math.random() * 20);
+
+document.write(r_text[i]);
+
